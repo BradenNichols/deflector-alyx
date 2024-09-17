d0c7b88207392ec1cde764dc6ed747b5969e1019
MUKILTEO\1719263 <1719263@mukilteo.wednet.edu>
2024-09-17 09:32:42 -0700

    yargh

--- Assets/Scripts/Components/GameControl.cs
+++ Assets/Scripts/Components/GameControl.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class GameControl : MonoBehaviour
 {
     public SpriteRenderer youDied;
+    public SpriteRenderer diedBG;
+
     private AudioSource deathSound;
 
     private GameObject timerObject;
@@ -35,17 +37,26 @@ public class GameControl : MonoBehaviour
 
         for (int i = 0; i <= max; i++)
         {
-            youDied.color = new Color(1, 1, 1, (float)i / max);
+            float ratio = (float)i / max;
+            youDied.color = new Color(1, 1, 1, ratio);
+
+            if (ratio <= 0.42f)
+                diedBG.color = new Color(0, 0, 0, ratio);
+
             yield return new WaitForSecondsRealtime(0.01f);
         }
 
         max = 150;
+
+        Vector3 baseSize = youDied.transform.localScale;
 
         for (int i = 0; i <= max; i++)
         {
             float ratio = 1 - ((float)i / max);
 
             youDied.color = new Color(ratio, ratio, ratio, 1);
+            youDied.transform.localScale = baseSize * (1 + ((float)i / max) / 8);
+
             yield return new WaitForSecondsRealtime(0.01f);
         }
 
