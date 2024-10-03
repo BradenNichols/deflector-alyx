9539590a0e42614b3e4110fdc9e015ea52af8007
MUKILTEO\1719263 <1719263@mukilteo.wednet.edu>
2024-10-03 10:16:23 -0700

    oopsies

--- Assets/Scripts/Components/DeathBrick.cs
+++ Assets/Scripts/Components/DeathBrick.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class DeathBrick : MonoBehaviour
 {
     public bool isActive = true;
+    public bool ignoreEnemies = false;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (isActive == false)
@@ -11,6 +13,11 @@ public class DeathBrick : MonoBehaviour
         Stats hitStats = collision.gameObject.GetComponent<Stats>();
 
         if (hitStats && !hitStats.isDead)
+        {
+            if (ignoreEnemies && !hitStats.isPlayer)
+                return;
+
             hitStats.Kill();
+        } 
     }
 }
